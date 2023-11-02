3f6d13c0a8a6b6fd8996696d8e481dceb08b64ac
tiny_bear <rebeccawilliams9941@gmail.com>
2023-11-02 12:34:10 +1100

    :recycle: Put prompt in main file so changes in one place.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import asyncio
 import os
 from api_request_parallel_processor import process_api_requests_from_file
-from generate_requests import generate_chat_completion_requests, prompt
+from generate_requests import generate_chat_completion_requests
 from save_generated_data_to_csv import save_generated_data_to_csv
 from data import data
 
 if __name__ == "__main__":
+    prompt = """
+        Write me a comprehensive and professional medical profile in 3rd person that is factual and starts with the professionals name and title without headings:
+        - The bio should be a minimum of 200 words.
+        - Use British spelling only, not American spelling.
+        - Never say [name] is a [gender].
+        - Use simple to the point language.
+        - Never say 'highly skilled' or 'highly experienced'.
+        - Never say 'as an...'
+        - Only mention the APHRA number briefly when you do mention it, and not at the start of the bio.
+        - The fields below have information you can use to create the content.
+        - Where the field answer is empty, do not include and do not include missing fields.
+        - Do not include information that is not completed.
+        """
     requests_filepath = "example_requests_to_chat_completion.jsonl"
     requests_output_filepath = "example_requests_to_chat_completion_results.jsonl"
     openai_endpoint = "https://api.openai.com/v1/chat/completions"
