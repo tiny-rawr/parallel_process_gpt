cfa3ac2386e1aae923a4c967542dae7032f4a6d7
tiny_bear <rebeccawilliams9941@gmail.com>
2023-11-17 22:12:01 +1100

    :construction: Parallel process more complex function calls.

--- generate_requests.py
+++ generate_requests.py
@@ -1,20 +1,5 @@
 import json
 
-
-def generate_chat_completion_requests(filename, data, prompt, model_name="gpt-3.5-turbo"):
-    with open(filename, "w") as f:
-        for x in data:
-            # Create a list of messages for each request
-            messages = [
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": str(x)}
-            ]
-
-            # Write the messages to the JSONL file
-            json_string = json.dumps({"model": model_name, "messages": messages})
-            f.write(json_string + "\n")
-
-
 def generate_gpt_request(prompt, data, functions=[]):
     model = "gpt-3.5-turbo"
     messages = [
