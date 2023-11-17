5129760e71f9cef76a0471aa522e23acc762af39
tiny_bear <rebeccawilliams9941@gmail.com>
2023-11-17 22:24:58 +1100

    :construction: Push temp example of function calling req.

--- generate_requests.py
+++ generate_requests.py
@@ -1,5 +1,20 @@
 import json
 
+def generate_chat_completion_requests(filename, data, prompt, model_name="gpt-3.5-turbo"):
+    with open(filename, "w") as f:
+        for x in data:
+            # Create a list of messages for each request
+            messages = [
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": str(x)}
+            ]
+
+            # Write the messages to the JSONL file
+            json_string = json.dumps({"model": model_name, "messages": messages})
+            f.write(json_string + "\n")
+
+# In progress: This is to generate more complex requests.
+# Pushing to main to share as example, but not used in rest of prog rn.
 def generate_gpt_request(prompt, data, functions=[]):
     model = "gpt-3.5-turbo"
     messages = [
